45a426906d213b6026bf0c40e4b37f7eb0e9c830
poxford3 <ptobmo@gmail.com>
2022-05-01 18:58:31 -0500

    started weather screen

--- screens/WeatherScreen.js
+++ screens/WeatherScreen.js
@@ -1,24 +1,49 @@
+import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native'
-import React from 'react'
 
 export default function WeatherScreen({ navigation, route }) {
 
-    const { city } = route.params
+    const { city } = route.params;
+    const [data, setData] = useState({});
+    const [icon, setIcon] = useState([]);
 
-    const cityName = JSON.stringify(city).replace(/\"/g,"")
+    const cityName = JSON.stringify(city).replace(/\"/g,"");
+
+    const api_key = "08ff95f3b0f97607b3b375dc9793f8b2";
+
+    useEffect(() => {
+        const getWeather = async () => {
+            try {
+                // console.log(lat," is lat and ",lng);
+                const response = (await fetch('https://api.openweathermap.org/data/2.5/weather?units=imperial&q=' + cityName + '&appid='+api_key+''));
+                const json = (await response.json());
+                setData(json.main);
+                setIcon(json.weather)
+                console.log(json.temp, 'get weather');
+            } catch (error) {
+                console.error(error);
+            } finally {
+           }
+        }
+        getWeather()
+    }, [])
 
     return (
-    <SafeAreaView styles={styles.container}>
+    <SafeAreaView style={styles.container}>
         <View style={styles.test}>
             <TouchableOpacity
             style={{backgroundColor: '#000', }}
-            // </View>onPress={navigation.navigate("Intro")}
+            onPress={() => navigation.goBack()}
             >
                 <Text style={{color:'white'}}>go back</Text>
             </TouchableOpacity>
-            <Text>WeatherScreen</Text>
-            <Text>{JSON.stringify(city)}</Text>
+            {/* <Text>{JSON.stringify(city)}</Text> */}
             <Text>{cityName}</Text>
+        </View>
+        <View style={styles.header}>
+            <Text style={styles.headerText}>
+                {cityName}
+            </Text>
         </View>
     </SafeAreaView>
   )
@@ -28,7 +53,15 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+    },
+    header: {
+        justifyContent: 'space-around'
+    },
+    headerText: {
+        fontSize: 40,
+        fontWeight: 'bold',
+        color: 'darkgrey'
     },
     test: {
         justifyContent: 'center',
